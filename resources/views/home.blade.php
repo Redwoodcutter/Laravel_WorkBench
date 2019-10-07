49ee27ae9802c5cc522817fc8fdd6b2d528b5d0b
Oguz Kumcular <oguzkumcular@gmail.com>
2019-10-08 00:41:44 +0300

    Dasboard Front-End Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,21 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+  <div class="row">
+      <div class="col-3 p-5">
+        <img src="https://avatars3.githubusercontent.com/u/11586702?s=460&v=4" class="rounded-circle" style="height: 200px;">
+      </div>
+      <div class="col-9 pt-5">
+        <div><h1>Redwoodcutter</h1></div>
+         <div class="d-flex">
+            <div class="pr-5"><strong>25</strong> Post</div>
+            <div class="pr-5"><strong>21k</strong> Followers</div>
+            <div class="pr-5"><strong>200</strong> Following</div>
         </div>
-    </div>
+        <div class="pt-4 font-weight-bold">RedwoodGram</div>
+        <div class="">Keelah Se'lai</div>
+        <div><a href="#">github.com/Redwoodcutter</a></div>
+      </div>
+    </div>      
 </div>
 @endsection
