529cbf0459b4db94c9c39f3e05e7f49cb95033a1
Oguz Kumcular <oguzkumcular@gmail.com>
2019-10-25 23:53:12 +0300

    İmage post view update.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,22 +2,42 @@
 
 @section('content')
 <div class="container">
-  <div class="row">
+  <form action="">
+    <div class="row">
         <div class="col-8 offset-2">
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">Post Caption</label>
+            <div class="form-group row">
+                <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}"  autocomplete="name" autofocus>
+                    <input id="caption" 
+                    type="text" 
+                    class="form-control @error('caption') is-invalid @enderror" 
+                    caption="caption" 
+                    value="{{ old('caption') }}"  
+                    autocomplete="caption" autofocus>
 
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+                    @error('caption')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                
+            </div>
+
+            <div class="row">
+                <label for="image" class="col-md-4 col-form-label">Post Image</label>
+
+                <input type="file", class="form-control-file" id="image" name="image">
+
+                @error('image')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                @enderror
+            </div>
+
         </div>
-  </div>
+    </div>
+  
+  </form>
 </div>
 @endsection
