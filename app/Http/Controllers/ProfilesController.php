07fd8f55cfcbc2b151e7ee377ddb4e669c84b9f3
Oguz Kumcular <oguzkumcular@gmail.com>
2019-11-06 02:45:42 +0300

    Edit profile page updated.

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -9,10 +9,10 @@ class ProfilesController extends Controller
 {
     public function index(User $user)
     {   
-        return view('profiles.index',compact('user'));
+        return view('profiles.index', compact('user'));
     }
     public function edit(User $user)
     {
-        return view('profiles.edit',compact('user'));
+        return view('profiles.edit', compact('user'));
     }
 }
--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -2,10 +2,51 @@
 
 @section('content')
 <div class="container">
-    <div class="container">
-        <div class="row">
-            
+<form action="/profile/ {{ $user->id }}" enctype="multipart/form-data" method="post">
+    @csrf
+    @method('PATCH')
+    <div class="row">
+        <div class="col-8 offset-2">
+            <div class="row">
+                <h1>Edit Profile</h1>
+            </div>
+
+            <div class="form-group row">  
+                <label for="title" class="col-md-4 col-form-label">Title</label>
+
+                    <input id="title" 
+                    type="text" 
+                    class="form-control @error('title') is-invalid @enderror" 
+                    name="title"
+                    value="{{ old('title') }}"  
+                    autocomplete="title" autofocus>
+
+                    @error('title')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                
+            </div>
+
+            <div class="row">
+                <label for="image" class="col-md-4 col-form-label">Post Image</label>
+
+                <input type="file", class="form-control-file" id="image" name="image">
+
+                @error('image')
+                   
+                        <strong>{{ $message }}</strong>
+                   
+                @enderror
+            </div>
+
+            <div class="row pt-4">
+                <button class="btn btn-primary">Add new post</button>
+            </div>
+
         </div>
     </div>
+  </form>
 </div>
 @endsection
